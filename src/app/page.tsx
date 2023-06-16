e0958d495bc9457977ff79b7ef7d1e237ca6b241
Johan van der Merwe <johannesmerwe@gmail.com>
2023-06-16 18:46:30 +0100

    Remove app router and add pages router

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-    return (
-        <main>
-            Welcome <Link href="/flashcards">flashcards</Link>
-        </main>
-    );
-}
